a15a99abb6e33cf04ae4a02a6d6431019eaeacac
dchavezr7 <dchavezr7@upao.edu.pe>
2026-05-09 18:42:44 -0500

    clase de superdron, y correcion de interface dron para un programa funcional

---
+++ src/droneadaptadordemo/SuperDrone.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package droneadaptadordemo;
+
+/**
+ *
+ * @author sistemas
+ */
+public class SuperDrone implements Drone {
+    public void beep() {
+        System.out.println("Beep Beep");
+    }
+    public void girarRotor() {
+        System.out.println("Rotor girando");
+    }
+    public void despegar() {
+        System.out.println("Drone despegando");
+    }
+}
